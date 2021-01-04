45e58cd285bd500859863339eae052a096f0d0ff
HeroPhil <philip.herold.01@gmail.com>
2021-01-04 15:35:06 +0100

    added first test of getmoviebyid

--- functions/test/test.offline.ts
+++ functions/test/test.offline.ts
@@ -2,11 +2,11 @@ import * as chai from 'chai';
 const assert = chai.assert;
 
 import * as sinon from 'sinon';
+import { stubDoc } from './stub';
 
 import * as firebaseFunctionsTest from 'firebase-functions-test';
 
-//import * as firebaseAdmin from 'firebase-admin';
-import * as firebaseAdmin from '../src/database/admin';
+import  * as admin from '../src/database/admin';
  
 import * as index from '../src/index';
 import { FeaturesList } from 'firebase-functions-test/lib/features';
@@ -18,7 +18,7 @@ describe('Cloud Functions', () => {
   let test: FeaturesList;
 
   before(() => {
-      adminInitStub = sinon.stub(firebaseAdmin, 'initializeApp');
+      adminInitStub = sinon.stub(admin.staticAdmin, 'initializeApp');
       myFunctions = index;
       test = firebaseFunctionsTest();
   });
@@ -29,61 +29,44 @@ describe('Cloud Functions', () => {
   });
 
   describe('getMovieByID', () => {
-    //let oldDatabase: { (): FirebaseFirestore.Firestore; (): FirebaseFirestore.Firestore; };
-    before(() => {
-      // Save the old database method so it can be restored after the test.
-      //oldDatabase = firebaseAdmin.firestore;
-    });
 
-    after(() => {
-      // Restoring admin.database() to the original method.
-      //firebaseAdmin.firestore = oldDatabase;
-    });
+    it('gets a Movie Object by a given ID', async () => {
 
-    it('gets a Movie Object by a given ID', (done) => {
-
-      const databaseStub = sinon.stub();
-      const docStub = sinon.stub();
-      const docParam = "/live/events/movies/1234567890";
-      const getStub = sinon.stub();
-
-      Object.defineProperty(firebaseAdmin, 'firestore', { get: () => databaseStub });
-      databaseStub.returns({ doc: docStub });
-      docStub.withArgs(docParam).returns({ get: getStub });
-      getStub.returns(Promise.resolve({
-        'cover': "gs://dhbw-kk-kino.appspot.com/live/events/movies/cover/2q0KTjjgsK2RNRg65OX6.jpg",
-        'description': "Fernab ihrer Heimat lauert ein weiteres Abenteuer auf die ungleiche Gruppe aus kurz und lang.",
-        'duration': 90,
-        'name': "Die unglaubliche Reise 2",
-        'priority': 20
+      stubDoc(
+        admin,
+        "live/events/movies/1234567890",
+        {
+          'cover': "gs://dhbw-kk-kino.appspot.com/live/events/movies/cover/2q0KTjjgsK2RNRg65OX6.jpg",
+          'description': "Fernab ihrer Heimat lauert ein weiteres Abenteuer auf die ungleiche Gruppe aus kurz und lang.",
+          'duration': 90,
+          'name': "Die unglaubliche Reise 2",
+          'priority': 20
         }
-      ));
-
-      const param = {
-        "id": 1234567890
-      };
+      );
 
       const wrappedFunction = test.wrap(myFunctions.database.getMovieByID);
 
-      let actual;
-      wrappedFunction(param).then((result: any) => {
-        actual = result;
-        done();
-      });
-      console.log(actual);
-      const expected = {};
+      const param = {
+        id: "1234567890"
+      };
+      const actual = await wrappedFunction(param);
 
-      const result = assert.equal(actual, expected);
-      console.log(result);
-      return result;
+      const expected = {
+        "id": param.id,
+        "data" : {
+          'cover': "gs://dhbw-kk-kino.appspot.com/live/events/movies/cover/2q0KTjjgsK2RNRg65OX6.jpg",
+          'description': "Fernab ihrer Heimat lauert ein weiteres Abenteuer auf die ungleiche Gruppe aus kurz und lang.",
+          'duration': 90,
+          'name': "Die unglaubliche Reise 2",
+          'priority': 20
+        }
+      };
+      assert.equal(actual.id, expected.id);
+      assert.deepEqual(actual.data, expected.data);
 
+      return;
 
     });
   });
-
-
-
-
-
 
 });
