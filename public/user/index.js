04a15a8060e66b86661b4736c2b05999068ae874
HeroPhil <philip.herold.01@gmail.com>
2021-01-04 15:54:48 +0100

    fixed merging again

---
+++ public/user/index.js
@@ -0,0 +1,97 @@
+// // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+// // The Firebase SDK is initialized and available here!
+//
+// firebase.auth().onAuthStateChanged(user => { });
+// firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+// firebase.firestore().doc('/foo/bar').get().then(() => { });
+// firebase.functions().httpsCallable('yourFunction')().then(() => { });
+// firebase.messaging().requestPermission().then(() => { });
+// firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+// firebase.analytics(); // call to activate
+// firebase.analytics().logEvent('tutorial_completed');
+// firebase.performance(); // call to activate
+let app;
+let functions;
+document.addEventListener("DOMContentLoaded", event => {
+    app = firebase.app();
+    functions = app.functions("europe-west1");
+});
+//
+// // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+function loadLastTickets(count) {
+    document.getElementById("tickets").innerHTML = "";
+    //getTickets
+    var tickets = ["", "", "", "", "", "", "", "", "", "", "", ""];
+    var displayCount = count;
+    if(count === -1 || count > tickets.length){
+        displayCount = tickets.length;
+    }
+    for (let index = 0; index < displayCount; index++) {
+        const element = tickets[index];
+        createTicket("Geiler Film", "7", "4", (index+1), "22.10.2021", "www.google.de")
+    }
+}
+
+function createTicket(title, hall, row, seat, date, value) {
+    var tickets = document.getElementById("tickets");
+    var ticket = document.createElement("div");
+    ticket.classList.add("ticket");
+    var ticketInformation = document.createElement("div");
+    ticketInformation.classList.add("ticketInformation");
+        var movieTitle = document.createElement("div");
+        movieTitle.classList.add("ticketMovieTitle");
+        movieTitle.innerHTML = title;
+    ticketInformation.appendChild(movieTitle);
+        var detailsTable = document.createElement("table");
+        var rowHall = document.createElement("tr");
+        var tHall = document.createElement("td");
+        tHall.innerHTML = "Saal";
+        var tHallValue = document.createElement("td");
+        tHallValue.innerHTML = hall;
+        rowHall.appendChild(tHall);
+        rowHall.appendChild(tHallValue);
+        detailsTable.appendChild(rowHall);
+        var rowRow = document.createElement("tr");
+        var tRow = document.createElement("td");
+        tRow.innerHTML = "Reihe";
+        var tRowValue = document.createElement("td");
+        tRowValue.innerHTML = row;
+        rowRow.appendChild(tRow);
+        rowRow.appendChild(tRowValue);
+        detailsTable.appendChild(rowRow);
+        var rowSeat = document.createElement("tr");
+        var tSeat = document.createElement("td");
+        tSeat.innerHTML = "Sitz";
+        var tSeatValue = document.createElement("td");
+        tSeatValue.innerHTML = seat;
+        rowSeat.appendChild(tSeat);
+        rowSeat.appendChild(tSeatValue);
+        detailsTable.appendChild(rowSeat);
+        var rowDate = document.createElement("tr");
+        var tDate = document.createElement("td");
+        tDate.innerHTML = "Datum";
+        var tDateValue = document.createElement("td");
+        tDateValue.innerHTML = date;
+        rowDate.appendChild(tDate);
+        rowDate.appendChild(tDateValue);
+        detailsTable.appendChild(rowDate);
+    ticketInformation.appendChild(detailsTable);
+    ticket.appendChild(ticketInformation);
+    tickets.appendChild(ticket);
+    createQrCode(ticket, value);
+}
+
+function createQrCode(element, textValue) {
+    var qrContainer = element.appendChild(document.createElement("div"));
+    qrContainer.classList.add("qr");
+    var qrcode = new QRCode(qrContainer, {
+        text: "https://www.google.de",
+        width: 128,
+        height: 128,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    });
+    qrcode.makeCode(textValue);
+}
