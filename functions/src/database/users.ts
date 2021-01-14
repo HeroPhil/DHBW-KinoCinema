31d2ec7a54cc8ff64e027877504ec1e9e5ad8b48
HeroPhil <philip.herold.01@gmail.com>
2021-01-14 18:21:37 +0100

    added user management functions

--- functions/src/database/users.ts
+++ functions/src/database/users.ts
@@ -1,8 +1,31 @@
 import { auth } from 'firebase-admin';
+import { CallableContext } from 'firebase-functions/lib/providers/https';
 import * as basics from './basics';
 
 const userCollectionPath = "live/users";
 const customersCollectionPath = userCollectionPath + "/customers";
+
+export class User {
+    id: string;
+    data: FirebaseFirestore.DocumentData;
+
+    constructor(id: string, data: FirebaseFirestore.DocumentData) {
+        this.id = id;
+        this.data = data;
+    }
+}
+
+interface UserInformation {
+    vorname?: string;
+    nachname?: string;
+    email?: string;
+    phone?: number;
+    zipCode?: number;
+    city?: string;
+    primaryAdress?: string;
+    secondaryAdress?: string;
+}
+
 
 export async function createNewUserInDatabase(user: auth.UserRecord){
     const data = {
@@ -12,4 +35,37 @@ export async function createNewUserInDatabase(user: auth.UserRecord){
 
     await basics.setDocumentByID(customersCollectionPath +"/"+ user.uid, data);
     return user.uid;
+}
+
+async function getInformationOfUserByID(uid: string) {
+    const doc = await basics.getDocumentByID(customersCollectionPath + "/" + uid);
+    return new User(doc.id, doc.data());   
+}
+
+
+export async function getInformationOfCurrentUser(context: CallableContext) {
+    if (context.auth.uid) {
+        return getInformationOfUserByID(context.auth.uid);
+    }
+    return {
+        error: {
+            message: "No user is sigend in"
+        }
+    };
+}
+
+async function editInformationOfUserByID(uid: string, changes: UserInformation) {
+    const x = await basics.updateDocumentByID(uid, changes);
+    return await basics.getDocumentByID(uid);    
+}
+
+export async function editInformationOfCurrentUser(context: CallableContext, changes: UserInformation) {
+    if (context.auth.uid) {
+        return editInformationOfUserByID(context.auth.uid, changes);
+    }
+    return {
+        error: {
+            message: "No user is sigend in"
+        }
+    };
 }
